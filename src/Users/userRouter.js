dcc216870bf496aa8fe5c5deaffe754ec35b44a2
Soomin <stro200123@gmail.com>
2023-06-26 19:19:43 +0900

    userRouter commit

--- src/Users/userRouter.js
+++ src/Users/userRouter.js
@@ -6,4 +6,9 @@ userRouter.get('/', (req, res) => {
   res.send('Hello');
 });
 
+userRouter.get('/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}. Welcome.`);
+});
+
 export default userRouter;
